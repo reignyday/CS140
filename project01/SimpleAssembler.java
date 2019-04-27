d2797a428545bd059b59d9c1241f5e70d1849be3
William Huebner <wilhueb@gmail.com>
2019-04-27 05:27:04 -0400

    error checking assembler

--- project01/SimpleAssembler.java
+++ project01/SimpleAssembler.java
@@ -7,6 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -63,29 +65,194 @@ public class SimpleAssembler implements Assembler
     @Override
     public int assemble(String inputFileName, String outputFileName, StringBuilder error)
     {
-        Map<Boolean, List<String>> lists = null;
+        if (error == null)
+            throw new IllegalArgumentException("Coding error: the error buffer is null");
 
-        try (Stream<String> lines = Files.lines(Paths.get(inputFileName)))
+        Map<Boolean, List<String>> lists = new HashMap<>();
+
+        lists.put(true, List.of());
+        lists.put(false, List.of());
+
+        int ret = 0;
+
+        try
         {
-            lists = lines.map(line -> line.trim())
-                         .filter(line -> line.length() > 0)
-                         .peek(line -> {
-                             if (line.toUpperCase().equals("DATA"))
-                                 this.readingCode = false;
-                         })
-                         .collect(Collectors.partitioningBy(line -> this.readingCode));
+            List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+
+            int blank = -1;
+
+            for (int i = 0; i < lines.size(); ++i)
+            {
+                String line = lines.get(i);
+
+                // 2
+                if (line.startsWith(" ") || line.startsWith("\t"))
+                {
+                    error.append("\nError on line " + (i+1) + 
+                            ": Line starts with illegal white space");
+
+                    ret = i + 1;
+                }
+
+                line = line.trim();
+
+                String[] parts = line.split("\\s+");
+
+                // 1
+                if (blank == -1 && line.length() == 0)
+                    blank = i;
+                else if (blank != -1 && line.length() != 0)
+                {
+                    error.append("\nError on line " + (blank+1) +
+                            ": Blank line in the source file");
+
+                    ret = blank;
+
+                    blank = -1;
+                }
+
+                if (line.length() == 0)
+                    continue;
+                
+                if (line.toUpperCase().equals("DATA"))
+                {
+                    // 3
+                    if (!this.readingCode)
+                    {
+                        error.append("\nError on line " + (i+1) +
+                                ": Cannot have two DATA separators");
+
+                        ret = i + 1;
+                    }
+
+                    if (!line.equals("DATA"))
+                    {
+                        error.append("\nError on line " + (i+1) +
+                                ": Line does not have DATA in upper case");
+
+                        ret = i + 1;
+                    }
+
+                    this.readingCode = false;
+
+                    continue;
+                }
+
+                if (this.readingCode)
+                {
+                    String opcode = parts[0];
+
+                    // 4
+                    if (!Instruction.OPCODES.containsKey(opcode.toUpperCase()))
+                    {
+                        error.append("\nError on line " + (i+1) + ": illegal mnemonic");
+
+                        ret = i + 1;
+                    }
+
+                    if (opcode != opcode.toUpperCase())
+                    {
+                        error.append("\nError on line " + (i+1) +
+                                ": mnemonic must be upper case");
+
+                        ret = i + 1;
+                    }
+
+                    // 5
+                    if (Assembler.noArgument.contains(opcode.toUpperCase()))
+                    {
+                        if (parts.length != 1)
+                        {
+                            error.append("\nError on line " + (i+1) +
+                                    ": this mnemonic cannot take arguments");
+
+                            ret = i + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (parts.length == 2)
+                        {
+                            // 6
+                            try
+                            {
+                                String arg;
+
+                                if (parts[1].startsWith("M") || parts[1].startsWith("N") ||
+                                        parts[1].startsWith("A"))
+                                    arg = parts[1].substring(1);
+                                else
+                                    arg = parts[1];
+
+                                Integer.parseInt(arg, 16);
+                            }
+                            catch (NumberFormatException e)
+                            {
+                                error.append("\nError on line " + (i+1) +
+                                        ": argument is not a hex number");
+
+                                ret = i + 1;
+                            }
+                        }
+                        else
+                        {
+                            error.append("\nError on line " + (i+1) +
+                                    ": this mnemonic has too many arguments");
+
+                            ret = i + 1;
+                        }
+                    }
+                }
+                else // this.readingCode == false
+                {
+                    // 7
+                    try
+                    {
+                        if (parts.length != 2)
+                        {
+                            error.append("\nError on line " + (i+1) +
+                                    ": DATA lines must be in the format [addr val]");
+
+                            ret = i + 1;
+
+                            continue;
+                        }
+
+                        Integer.parseInt(parts[0], 16);
+                        Integer.parseInt(parts[1], 16);
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        error.append("\nError on line " + (i + 1) +
+                                ": data has non-numeric memory address");
+
+                        ret = i + 1;
+                    }
+                }
+
+                var l = new ArrayList<String>(lists.get(this.readingCode));
+                l.add(line);
+                lists.put(this.readingCode, l);
+            }
 
             System.out.println("true list " + lists.get(true));
             System.out.println("false list " + lists.get(false));
         }
+        catch (FileNotFoundException e)
+        {
+            error.append("\nError: Unable to open the input file");
+
+            return -1;
+        }
         catch (IOException e)
         {
-            e.printStackTrace();
+            error.append("\nUnexplained IO Exception");
 
             return -1;
         }
 
-        lists.get(false).remove("DATA");
+        if (ret != 0)
+            return ret;
 
         List<Instruction> outputCode = lists.get(true).stream()
                                         .map(line -> line.split("\\s+"))
@@ -121,9 +288,15 @@ public class SimpleAssembler implements Assembler
 
             out.close();
         }
+        catch (FileNotFoundException e)
+        {
+            error.append("\nError: Unable to write the assembled program to the output file");
+
+            return -1;
+        }
         catch (IOException e)
         {
-            e.printStackTrace();
+            error.append("\nUnexplained IO Exception");
 
             return -1;
         }
@@ -145,6 +318,7 @@ public class SimpleAssembler implements Assembler
                                 filename + ".pexe", error);
 
             System.out.println("result = " + i);
+            System.out.println(error);
         }
     }
 }
