8c8b1040086abc04083446816b744a86350ef083
William Huebner <wilhueb@gmail.com>
2019-04-29 15:56:08 -0400

    added main method to FullAssembler for testing

--- project01/src/project/FullAssembler.java
+++ project01/src/project/FullAssembler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class FullAssembler implements Assembler
 {
@@ -181,5 +182,21 @@ public class FullAssembler implements Assembler
             return new SimpleAssembler().assemble(inputFileName, outputFileName, error);
 
         return ret;
+    }
+
+    public static void main(String[] args)
+    {
+        StringBuilder error = new StringBuilder();
+
+        System.out.print("Enter the name of the file without extension: ");
+
+        try (Scanner keyboard = new Scanner(System.in))
+        {
+            String filename = keyboard.nextLine();
+
+            int i = new FullAssembler().assemble(filename + ".pasm", filename + ".pexe", error);
+
+            System.out.println("result = " + i);
+        }
     }
 }
