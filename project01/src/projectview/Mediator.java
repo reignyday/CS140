cd544ea56af923973629cee3e04fc901b51da8c5
William Huebner <wilhueb@gmail.com>
2019-04-29 03:39:45 -0400

    code formatting

--- project01/src/projectview/Mediator.java
+++ project01/src/projectview/Mediator.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 import project.Machine;
 import project.Memory;
@@ -32,85 +33,85 @@ public class Mediator
         this.controlPanel.update();
         this.processorPanel.update();
     }
-    
+
     private void createAndShowGUI()
     {
         this.tUnit = new TimerUnit(this);
-        
+
         //this.ioUnit = new IOUnit(this);
         //this.ioUnit.initialize();
-        
-        codeViewPanel = new CodeViewPanel(machine);
-        memoryViewPanel1 = new MemoryViewPanel(machine, 0, 160);
-        memoryViewPanel2 = new MemoryViewPanel(machine, 160, Memory.DATA_SIZE / 2);
-        memoryViewPanel3 = new MemoryViewPanel(machine, Memory.DATA_SIZE / 2, Memory.DATA_SIZE);
-        controlPanel = new ControlPanel(this);
-        processorPanel = new ProcessorViewPanel(machine);
-        
+
+        this.codeViewPanel = new CodeViewPanel(this.machine);
+        this.memoryViewPanel1 = new MemoryViewPanel(this.machine, 0, 160);
+        this.memoryViewPanel2 = new MemoryViewPanel(this.machine, 160, Memory.DATA_SIZE / 2);
+        this.memoryViewPanel3 = new MemoryViewPanel(this.machine, Memory.DATA_SIZE / 2,
+                Memory.DATA_SIZE);
+        this.controlPanel = new ControlPanel(this);
+        this.processorPanel = new ProcessorViewPanel(this.machine);
+
         //this.menuBuilder = new MenuBarBuilder(this);
-        
+
         this.frame = new JFrame("Simulator");
-        
+
         //JMenuBar bar = new JMenuBar();
-        
+
         //this.frame.setJMenuBar(bar);
-        
+
         //bar.add(menuBuilder.createFileMenu());
         //bar.add(menuBuilder.createExecuteMenu());
 
-        Container content = frame.getContentPane(); 
-        
-        content.setLayout(new BorderLayout(1,1));
+        Container content = this.frame.getContentPane();
+
+        content.setLayout(new BorderLayout(1, 1));
         content.setBackground(Color.BLACK);
-        
-        frame.setSize(1200,600);
-        
-        frame.add(codeViewPanel.createCodeDisplay(), BorderLayout.LINE_START);
-        frame.add(processorPanel.createProcessorDisplay(), BorderLayout.PAGE_START);
-        
+
+        this.frame.setSize(1200, 600);
+
+        this.frame.add(this.codeViewPanel.createCodeDisplay(), BorderLayout.LINE_START);
+        this.frame.add(this.processorPanel.createProcessorDisplay(), BorderLayout.PAGE_START);
+
         JPanel center = new JPanel();
-        
-        center.setLayout(new GridLayout(1,3));
-        
-        center.add(memoryViewPanel1.createMemoryDisplay());
-        center.add(memoryViewPanel2.createMemoryDisplay());
-        center.add(memoryViewPanel3.createMemoryDisplay());
-        
+
+        center.setLayout(new GridLayout(1, 3));
+
+        center.add(this.memoryViewPanel1.createMemoryDisplay());
+        center.add(this.memoryViewPanel2.createMemoryDisplay());
+        center.add(this.memoryViewPanel3.createMemoryDisplay());
+
         this.frame.add(center, BorderLayout.CENTER);
-        this.frame.add(controlPanel.createControlDisplay(), BorderLayout.PAGE_END);
-        
+        this.frame.add(this.controlPanel.createControlDisplay(), BorderLayout.PAGE_END);
+
         // the next line will be commented or deleted later
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //this.frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         //this.frame.addWindowListener(WindowListenerFactory.windowClosingFactory(e -> exit()));
-        
+
         this.frame.setLocationRelativeTo(null);
-        
+
         this.tUnit.start();
-        
+
         //this.currentState().enter();
-        
+
         this.frame.setVisible(true);
-        
-        notify("");
+
+        this.notify("");
     }
-    
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run()
-            {
-                Mediator mediator = new Mediator();
-                
-                Machine machine = new Machine(() -> mediator.setCurrentState(States.PROGRAM_HALTED));
-                
-                mediator.setMachine(machine);
-                
-                mediator.createAndShowGUI();
-            }
+
+    public static void main(String[] args)
+    {
+        javax.swing.SwingUtilities.invokeLater(() ->
+        {
+            Mediator mediator = new Mediator();
+
+            Machine machine = new Machine(
+                    () -> mediator.setCurrentState(States.PROGRAM_HALTED));
+
+            mediator.setMachine(machine);
+
+            mediator.createAndShowGUI();
         });
     }
 
-    
     public void step()
     {
     }
